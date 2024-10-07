3005808e29d27b69ee398f35455d0f92ad96b8bb
BobbyBob <116626332+RenoSebastian@users.noreply.github.com>
2024-10-07 13:34:16 +0700

    add data clock_in and clock_out to absensi table

--- app/Entities/Members.php
+++ app/Entities/Members.php
@@ -1,55 +1,6 @@
 <?php
+
 namespace App\Entities;
-
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
-
-class Members extends Model
-{
-    protected $connection = 'pgsql_ardi';
-    protected $table = 'public.members';
-    protected $primaryKey = 'id';
-    public $timestamps = false;
-
-    protected $fillable = [
-        'id',
-        'balance',
-        'fullname',
-        'phone',
-        'email',
-        'description',
-        'status',
-        'created_by',
-        'updated_by',
-        'created_at',
-        'updated_at',
-        'deleted_at',
-        'card_number',
-        'opt_1',
-        'opt_2',
-        'opt_3',
-        'opt_4',
-        'opt_5',
-        'is_free',
-        'partner_acc_no',
-        'pin_state',
-        'failed_pin_trial',
-        'issuer_id',
-        'issuer_partner_id',
-    ];
-
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::creating(function ($model) {
-            if (empty($model->{$model->getKeyName()})) {
-                $model->{$model->getKeyName()} = (string) Str::uuid();
-            }
-        });
-    }
-
-    namespace App\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -95,6 +46,3 @@ class Members extends Model
         'id' => 'uuid',  // Pastikan id dicasting sebagai UUID
     ];
 }
-
-}
-
