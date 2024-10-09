4995dcfa4f02571e14f2c39b98541abe607a7764
fathiaqurrata <123913816+fathiaqurrata@users.noreply.github.com>
2024-10-09 13:39:55 +0700

    loc

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\NfcController;
+use App\Http\Controllers\LocationController;
 
 Route::post('auth/login', [LoginController::class, 'loginApk']);
 
 Route::post('/members/checkin', [NfcController::class, 'checkin']);
 Route::post('/members/checkout', [NfcController::class, 'checkout']);
+Route::post('member/locations', [LocationController::class, 'getLocations'])->name('locations.index');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
