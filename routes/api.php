6cd4f369d594317ed065b1e40c2c8b71f97ef787
fathiaqurrata <123913816+fathiaqurrata@users.noreply.github.com>
2024-10-09 11:45:09 +0700

    ..

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\NfcController;
 
 Route::post('auth/login', [LoginController::class, 'loginApk']);
