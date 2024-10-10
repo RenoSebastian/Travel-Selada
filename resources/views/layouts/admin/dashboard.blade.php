4ef88560c6280dfb9dd437f62b213023dc638ab1
BobbyBob <116626332+RenoSebastian@users.noreply.github.com>
2024-10-10 12:05:23 +0700

    -

--- resources/views/layouts/admin/dashboard.blade.php
+++ resources/views/layouts/admin/dashboard.blade.php
@@ -6,9 +6,6 @@
 @section('content')
     <h1>Admin Dashboard</h1>
     <p>Welcome to the Admin dashboard, where you can manage transactions, terminals, and agents.</a>
-
-    <a href="{{ route('location.create') }}" class="btn btn-primary">Input Data Brand</a>
-    <a href="{{ route('users.create') }}" class="btn btn-primary">Input User</a>
     
     @if(session('error'))
         <div class="alert alert-danger">
