fd18727aa6c07171f4981818807c67586d9458c2
BobbyBob <116626332+RenoSebastian@users.noreply.github.com>
2024-10-14 21:50:52 +0700

    -

--- resources/views/layouts/user/dashboard.blade.php
+++ resources/views/layouts/user/dashboard.blade.php
@@ -5,4 +5,7 @@
 @section('content')
     <h1>Tour Leader Dashboard</h1>
     <p>selamat datang Tour leader.</p>
+    <li class="nav-item">
+        <a href="{{ route('tour_leader.index') }}" class="btn btn-custom">data peserta tour</a>
+    </li>
 @endsection
